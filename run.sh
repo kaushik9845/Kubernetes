ba416a0519160b2669c3ae4b4098109cced119dc
kaushik9845 <kaushik9845@gmail.com>
2022-03-08 10:26:38 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+kubectl apply -f Kubernates/ConfigMap.yml
+kubectl apply -f Kubernates/Secrets.yml
+kubectl apply -f Kubernates/deployment.yml
+kubectl apply -f Kubernates/DBService.yml
+kubectl apply -f Kubernates/SpringbootAppDeployment.yml
